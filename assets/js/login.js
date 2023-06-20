0993dfb258cc0d484c395f4e9e0cfae1d3eecb6e
Your Name <you@example.com>
2023-06-20 16:12:13 +0400

    added header,services,choose,footer sections to the services page

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,90 @@
-let loginForm = document.querySelector(".my-form");
+// let loginForm = document.querySelector(".my-form");
 
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let email = document.getElementById("email");
-    let password = document.getElementById("password");
+// loginForm.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     let email = document.getElementById("email");
+//     let password = document.getElementById("password");
 
-    console.log('Email:', email.value);
-    console.log('Password:', password.value);
-    // process and send to API 
-});
+//     console.log('Email:', email.value);
+//     console.log('Password:', password.value);
+//     // process and send to API
+// });
+
+let emailLogin = document.querySelector("#login-email");
+// let passwordLogin = document.querySelector("#login-password");
+let passwordLogin = document.querySelector("#password");
+let btnLogin = document.querySelector("#login-btn");
+let form = document.querySelector("form");
+let BASE_URL_USERS = "http://localhost:8080/users";
+
+async function getDataUsers() {
+  let res = await axios.get(BASE_URL_USERS);
+  let data = res.data;
+//   console.log(data);
+}
+
+getDataUsers()
+
+
+// form.addEventListener("submit", async function () {
+//   let res = await axios.get(BASE_URL_USERS);
+//   let data = res.data;
+//   console.log(data);
+//   data.find((item) => {
+//     if (
+//       item.isAdmin &&
+//       emailLogin.value == "rashid@gmail.com" &&
+//       passwordLogin.value == 123456
+//     ) {
+//       window.location = `/admin-panel/admin.html`;
+//     } else if (
+//       data.filter(
+//         (item) =>
+//           item.email == emailLogin.value && item.password == passwordLogin.value
+//       )
+//     ) {
+//       window.location = `index.html`;
+//     } else {
+//       alert("Pls Sign Up!");
+//     }
+//   });
+// });
+
+
+// btnLogin.addEventListener("click", async function(){
+//     let res = await axios.get(BASE_URL_USERS);
+//     let data = res.data;
+//     console.log(data);
+//     data.find((item) => {
+//         if (
+//           item.isAdmin &&
+//           emailLogin.value == "rashid@gmail.com" &&
+//           passwordLogin.value == 123456
+//         ) {
+//           window.location = `index.html`;
+//         } else if (
+//           data.find(
+//             (item) =>
+//               item.email == emailLogin.value && item.password == passwordLogin.value
+//           )
+//         ) {
+//           window.location = `index.html`;
+//         } else {
+//           alert("Pls Sign Up!");
+//         }
+//       });
+// })
+
+btnLogin.addEventListener("click", async function () {
+    let res = await axios.get(BASE_URL_USERS);
+    let data = res.data;
+    data.find((user) => {
+      if (user.isAdmin && emailLogin.value=="aliyev@gmail.com" && passwordLogin.value==232323) {
+        window.location = `/admin-panel/admin.html`;
+      } else if(data.filter((user)=>user.email==emailLogin.value && user.password==passwordLogin.value)){
+        window.location = `index.html`;
+      }else{
+        alert("Pls Sign Up!")
+      } 
+    });
+  });
--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,69 @@ function nav(){
       burger.remove.toggle("collapsed")
   })
 }
+
+const comp=document.querySelector('.team-dinamik')
+const load=document.querySelector(".load")
+let filtered=[]
+let copyData=[]
+let defaultArr=[]
+let num=4
+let favData
+let BASE_URL="http://localhost:8080/workers"
+let BASE_URL2=" http://localhost:8080/favs"
+
+async function getDataUsers() {
+  let res = await axios.get(BASE_URL);
+  let data = res.data;
+  // console.log(data);
+  getCardWorker(data)
+}
+
+getDataUsers()
+
+function getCardWorker(arr){
+  comp.innerHTML=""
+  arr.forEach(element => {
+    comp.innerHTML+=`
+    <div
+              class="col col-md-3 col-lg-3 p-2"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay="100"
+            >
+              <div class="card-work" style="width: 18rem">
+                <img
+                  src="${element.img}"
+                  class="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h3 class="card-title text-center">${element.employee}</h3>
+                  <p class="card-text text-center">CEO, Founder & Developer</p>
+                </div>
+              </div>
+
+              <span class="team-wrap">
+                
+                <ul class="team-icons">
+                  <li>
+                    <a href="#"><i class="p-2 fa-brands fa-twitter"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="p-2 fa-brands fa-facebook-f"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="p-2 fa-brands fa-instagram"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"
+                      ><i class="p-2 fa-brands fa-google-plus-g"></i
+                    ></a>
+                  </li>
+                </ul>
+        
+              </span>
+            </div>
+    `
+  });
+}
